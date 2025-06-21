be62579562d55da6bb277fca01c2768943f80487
clementine-code <57307815+clementine-code@users.noreply.github.com>
2025-06-21 16:28:22 -0500

    Fix scroll to top on navigation

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { DefaultPageLayout } from "@/ui/layouts/DefaultPageLayout";
 import PasswordProtection from "./components/PasswordProtection";
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Sell from "./pages/Sell";
@@ -14,6 +15,7 @@ function App() {
   return (
     <PasswordProtection>
       <Router>
+        <ScrollToTop />
         <DefaultPageLayout>
           <Routes>
             <Route path="/" element={<Home />} />
