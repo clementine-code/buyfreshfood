bb85c4ac9bfddd6ccea9cd890505c33f11605590
clementine-code <57307815+clementine-code@users.noreply.github.com>
2025-06-29 22:35:00 -0500

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -194,7 +194,13 @@ function Home() {
                 <div className="flex w-full min-w-[320px] grow shrink-0 basis-0 items-start gap-4 mobile:items-center mobile:justify-center">
                   <img
                     className="h-5 w-5 flex-none object-cover"
-                    src="https://res.cloudinary.com/subframe/image/upload/v1711417507/shared/y2rsnhq3mex4auk54aye.png"
+                    <Link to="/">
+              <img
+                className="h-10 flex-none object-contain"
+                src="/Buy-Fresh-Food-Logo.png"
+                alt="Buy Fresh. Food Logo"
+              />
+            </Link>
                   />
                   <span className="grow shrink-0 basis-0 font-['Inter'] text-[14px] font-[500] leading-[20px] text-default-font -tracking-[0.01em]">
                     BuyFresh.Food
